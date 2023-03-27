eb93f343d5a27f69d47c38ab74ec9b29080da508
Shirley Fang <fangyixuan0123@gmail.com>
2023-03-27 12:46:02 -0400

    Set the snake moving down with fixed length.

--- main.py
+++ main.py
@@ -35,11 +35,41 @@ class Food:
 
         Canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = FOOD_COLOR, tag = "food")
 
-def next_turn():
-    pass
+def next_turn(snake, food):
+    
+    x , y = snake.coordinates[0]
+
+    if direction == "up":
+        y -= SPACE_SIZE
+    elif direction == "down":
+        y += SPACE_SIZE
+    elif direction == "left":
+        x -= SPACE_SIZE
+    elif direction == "right":
+        x += SPACE_SIZE
+
+    snake.coordinates.insert(0,(x, y))
+    #adding the snake head to the coordinate list.
+
+    square = Canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = SNAKE_COLOR)
+
+    snake.squares.insert(0, square)
+
+    del snake.coordinates[-1]
+
+    Canvas.delete(snake.squares[-1])
+
+    del snake.squares[-1]
+
+    #delete the last element of the snake from the list and canvas. 
+
+    window.after(SPEED, next_turn, snake,food)
 
 def change_direction(new_direction):
-    pass
+    
+    global direction 
+    # old direction
+
 
 def check_collisions():
     pass
@@ -52,7 +82,7 @@ window.title("APPLE EATER")
 window.resizable(False, False)
 
 score = 0
-dirction = 'down'
+direction = 'down'
 
 label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
 label.pack()
@@ -72,7 +102,14 @@ y = int((screen_height/2) - (window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+window.bind('<Left>', lambda event: change_direction('left'))
+window.bind('<Right>', lambda event: change_direction('right'))
+window.bind('<Up>', lambda event: change_direction('up'))
+window.bind('<Down>', lambda event: change_direction('down'))
+
 snake = Snake()
 food = Food()
+
+next_turn (snake, food)
 
 window.mainloop()   
