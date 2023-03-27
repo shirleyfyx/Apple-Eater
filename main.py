93727df9de8e20861fb0465fd881666b681c8381
Shirley Fang <fangyixuan0123@gmail.com>
2023-03-27 02:04:32 -0400

    Worked on the Food class.

--- main.py
+++ main.py
@@ -14,7 +14,15 @@ class Snake:
     pass
 
 class Food:
-    pass
+    
+    def __init__(self):
+
+        x = random.randint(0, (GAME_WIDTH/SPACE_SIZE)-1) * SPACE_SIZE
+        Y = random.randint(0, (GAME_HEIGHT/SPACE_SIZE)-1) * SPACE_SIZE
+    
+        self.coordinates = [x, y]
+
+        Canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = FOOD_COLOR, tag = "food")
 
 def next_turn():
     pass
@@ -30,7 +38,7 @@ def game_over():
 
 window = Tk()
 window.title("APPLE EATER")
-window.resizable(True, True)
+window.resizable(False, False)
 
 score = 0
 dirction = 'down'
@@ -52,5 +60,8 @@ x = int((screen_width/2) - (window_width/2))
 y = int((screen_height/2) - (window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
 
 window.mainloop()   
